b78701741d86d36868a3d5dbecd55302e48a03c8
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-07-03 14:41:51 -0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import signal
 from PySide6 import QtWidgets
 from PySide6.QtCore import QCoreApplication
@@ -7,22 +6,16 @@ from ui.telaEmpresa import EmpresaWindow
 from utils.icone import usar_icone
 
 def main():
-    inicio = time.time()
-
     app = QtWidgets.QApplication(sys.argv)
     app.aboutToQuit.connect(lambda: print("Sistema encerrando..."))
 
     janela = EmpresaWindow()
     usar_icone(janela)
-    janela.showMaximized()
-
-    fim = time.time()
-    print(f"[DEBUG] Tempo de abertura da janela inicial: {fim - inicio:.2f} segundos")
+    janela.show()
 
     sys.exit(app.exec())
 
 def sinal_encerramento(sig, frame):
-    print("Encerrando o sistema por sinal (Ctrl+C ou kill)...")
     QCoreApplication.quit()
 
 signal.signal(signal.SIGINT, sinal_encerramento)
